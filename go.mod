0516030a6ba24d8f79d9b37c4fce8ad6fd6512c8
mew <w3xse7en@outlook.com>
2020-11-25 22:12:02 +0800

    add AccessProjB

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/w3xse7en/proj-a
 
 go 1.15
 
-require github.com/w3xse7en/third-proj-c v0.0.0-20201125135521-687458227162
+require (
+	github.com/w3xse7en/proj-b v0.0.0-20201125140236-2e2e83bbdf89
+	github.com/w3xse7en/third-proj-c v0.0.0-20201125135521-687458227162
+)
--- pkg/proja/rpc.go
+++ pkg/proja/rpc.go
@@ -2,9 +2,14 @@ package proja
 
 import (
 	"fmt"
+	"github.com/w3xse7en/proj-b/pkg/projb"
 	c "github.com/w3xse7en/third-proj-c"
 )
 
 func RpcA(i int) string {
 	return fmt.Sprintf("im rpc a input int is %v", c.ToolCItoa(i))
+}
+
+func AccessProjB(i int) string {
+	return projb.RpcB(i)
 }
